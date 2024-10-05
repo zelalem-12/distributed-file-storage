f7717dbe3fe6cf04d62373562aa07e96f0356e43
Zelalem A. Enyew <zelalem@MacBook-Pro.local>
2024-10-06 01:49:27 +0300

    inial commit and simple file upload

---
+++ cmd/main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/google/uuid"
+)
+
+type FileUpload struct {
+	Id   uuid.UUID `json:"id"`
+	Name string    `json:"name"`
+	Path string    `json:"path"`
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./static/index.html")
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Parse the Input, Type multipart/form-data
+	if err := r.ParseMultipartForm(10 << 20); err != nil {
+		http.Error(w, "Failed to parse multipart form: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	//Retrive file from form data
+
+	file, handler, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "Failed to retrieve file from form-data: "+err.Error(), http.StatusInternalServerError)
+		fmt.Printf("Failed to retrieve file from form-data: %s", err.Error())
+		return
+	}
+	defer file.Close()
+
+	// fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	// fmt.Printf("File Size: %+v", handler.Size)
+	// fmt.Printf("File Header: %+v", handler.Header)
+
+	//Create a file and write the contents of uploaded file to it
+
+	extention := filepath.Ext(handler.Filename)
+
+	fmt.Println("file extention: " + extention)
+
+	fileName := strings.TrimSuffix(handler.Filename, extention)
+
+	newFileFullName := fmt.Sprintf("%s_*%s", fileName, extention)
+
+	fmt.Println("New file FullName: " + newFileFullName)
+
+	newFile, err := os.CreateTemp("uploads", newFileFullName)
+	if err != nil {
+		http.Error(w, "Failed to create a new file on server: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer newFile.Close()
+
+	uploadedFileContentInBytes, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, "Failed to create a new file on server: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	newFile.Write(uploadedFileContentInBytes)
+
+	// Sample data to be written to JSON
+	filePath := newFile.Name()
+	newFileUpload := FileUpload{
+		Id:   uuid.New(),
+		Name: strings.Split(filePath, "/")[1],
+		Path: filePath,
+	}
+
+	// Read the existing JSON file
+	jsonFileName := "file.json"
+	var files []FileUpload
+
+	if _, err := os.Stat(jsonFileName); err == nil {
+		// File exists, read it
+
+		jsonFileData, err := os.ReadFile(jsonFileName)
+		if err != nil {
+			http.Error(w, "Error reading json file: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Unmarshal the existing data into the Go slice
+		err = json.Unmarshal(jsonFileData, &files)
+		if err != nil {
+			http.Error(w, "Error unmarshaling JSON: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+	}
+
+	// Append the new data
+	files = append(files, newFileUpload)
+
+	// Convert the Go data structure to JSON
+	updatedJsonData, err := json.MarshalIndent(files, "", " ")
+	if err != nil {
+		http.Error(w, "Error marshalling  updated json data: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Write the updated JSON data back to the file
+	err = os.WriteFile(jsonFileName, updatedJsonData, 0644)
+	if err != nil {
+		http.Error(w, "Error writing updated json to file: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprint(w, "File Uploaded Successfully \n")
+}
+
+func SetupRoutes() {
+	serverPort := 8080
+	addr := fmt.Sprintf(":%d", serverPort)
+	http.HandleFunc("/", HomeHandler)
+	http.HandleFunc("/upload", UploadHandler)
+	http.ListenAndServe(addr, nil)
+}
+
+func main() {
+	SetupRoutes()
+}
---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/zelalem-12/distributed-file-storage
+
+go 1.23
+
+require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	gorm.io/driver/mysql v1.5.7 // indirect
+	gorm.io/gorm v1.25.12 // indirect
+)
